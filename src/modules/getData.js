69ff3ca7050f04d01d21e3d6f0a7490f85578572
Vladislav Yudakov <skiff2140@gmail.com>
2023-08-11 15:43:33 +0400

    first commit

---
+++ src/modules/getData.js
@@ -0,0 +1,9 @@
+const getData = () =>{
+    return fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => {
+        return response.json()
+    })
+     
+}
+
+export default getData
