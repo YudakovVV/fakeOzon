eaa5de3f5da25aeccbb8838e9416275f0eb1de3f
Vladislav Yudakov <skiff2140@gmail.com>
2023-08-14 15:07:07 +0400

    Получение данных из файлов БД и реализация поиска

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -1,5 +1,6 @@
-const getData = () =>{
-    return fetch('https://fakeozon-2c5dd-default-rtdb.firebaseio.com/goods.json')
+const getData = (str) =>{
+    return fetch(`https://fakeozon-2c5dd-default-rtdb.firebaseio.com/goods.json`)
+    //return fetch(`https://fakeozon-2c5dd-default-rtdb.firebaseio.com/goods.json?${str ? `search=${str}` : ''}`)
     .then((response) => {
         return response.json()
     })
