29cd7ca8748f7c4ba559f77ef52c82d47cc16845
Vladislav Yudakov <skiff2140@gmail.com>
2023-08-14 16:48:57 +0400

    фильтр и корзина

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -2,6 +2,7 @@ const cart = () => {
     const cartBtn = document.getElementById('cart')
     const cartModal = document.querySelector('.cart')
     const cartCloseBtn = cartModal.querySelector('.cart-close')
+    const goodsWrapper = document.querySelector('.goods')
 
     const openCart = () => {
         cartModal.style.display = 'flex'
@@ -13,6 +14,22 @@ const cart = () => {
 
     cartBtn.addEventListener('click', openCart)
     cartCloseBtn.addEventListener('click', closeCart)
+
+    goodsWrapper.addEventListener('click', (event) => {
+        if(event.target.classList.contains('btn-primary')){
+            const card = event.target.closest('.card')
+            const key = card.dataset.key
+            const goods = JSON.parse(localStorage.getItem('goods'))
+            const cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
+            const goodItem = goods.find((item) => {
+                return item.id === +key
+            })
+            cart.push(goodItem)
+            localStorage.setItem('cart', JSON.stringify(cart))
+            
+            
+        }
+    })
     
 } 
 
