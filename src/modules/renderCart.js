6ee68a66e781586b44731bf9ef5897615b58efe8
Vladislav Yudakov <skiff2140@gmail.com>
2023-08-14 16:48:38 +0400

    фильтр и корзина

---
+++ src/modules/renderCart.js
@@ -0,0 +1,29 @@
+const renderCart = (goods) => {
+    const goodsWrapper = document.querySelector('.cart-wrapper')
+
+	localStorage.setItem('goods', JSON.stringify(goods))
+
+    cartWrapper.innerHTML = ''
+
+    
+
+    goods.forEach((goodItem) => {
+        cartWrapper.insertAdjacentHTML('beforeend', `
+            <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+                <div class="card" data-key="${goodItem.id}">
+					${goodItem.sale ? '<div class="card-sale">🔥Hot Sale🔥</div>' : ''}
+						<div class="card-img-wrapper">
+							<span class="card-img-top"
+								style="background-image: url('${goodItem.img}')"></span>
+						</div>
+					    <div class="card-body justify-content-between">
+						    <div class="card-price">${goodItem.price} ₽</div>
+							    <h5 class="card-title">${goodItem.title}</h5>
+							    <button class="btn btn-primary">В корзину</button>
+						    </div>
+					    </div>
+			    </div>
+        `)
+    });
+}
+export default renderCart
